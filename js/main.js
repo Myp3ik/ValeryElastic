39ea16a618cab9a02488da6ed2e1c48dadcfc836
Alexander Murza <murza.alexander@gmail.com>
2016-10-02 17:10:12 +0200

    removed trash; added simple test js

---
+++ js/main.js
@@ -0,0 +1,18 @@
+setLang(0); //Englsh
+
+
+function setLang(langId) {
+    if (langId === 0) {
+        $("#lang0").hide();
+        $("#lang1").show();
+        $("#lang2").show();
+    } else if (langId === 1) {
+        $("#lang0").show();
+        $("#lang1").hide();
+        $("#lang2").show();
+    } else if (langId === 2) {
+        $("#lang0").show();
+        $("#lang1").show();
+        $("#lang2").hide();
+    }
+}
